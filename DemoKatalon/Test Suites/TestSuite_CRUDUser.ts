3d073fb1559b60e15dc1a5e88852f9daf5fdcea4
Minh Thùy Trần <56989794+thuyttm2-0617@users.noreply.github.com>
2020-06-21 17:15:46 +0700

    Add files via upload

---
+++ DemoKatalon/Test Suites/TestSuite_CRUDUser.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_CRUDUser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5a522171-8651-402b-b5e7-96260bedcf6b</testSuiteGuid>
+   <testCaseLink>
+      <guid>8c402689-8af5-40cb-852b-d2c3afa35226</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CRUDUser_Demo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>572895ce-4c3a-4a1d-8c1e-f920ed845b1d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CaculatorTest/Addition</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
